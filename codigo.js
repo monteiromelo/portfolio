91183772fdb3b66da43bac8d657eff0281ed7c86
monteiromelo <39968049+monteiromelo@users.noreply.github.com>
2018-06-19 11:46:35 -0300

    portfolio

---
+++ codigo.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $("#enviar").click(function(){
+    var nome = $("#nome").val(); 
+    var email = $("#email").val();
+    var mensagem = $("#msg").val();
+
+    if (nome != "" && email != "" && mensagem != "") {
+     alert("Seu email foi enviado com sucesso!");
+     $("#nome").val(""); 
+     $("#email").val("");
+     $("#msg").val("");
+    } else {
+      alert("Por favor, preencha todos os campos para efetuar o envio do email!");
+    }
+  });
+
+  $("#instagram").mouseenter(function(){
+    $("#instagram").animate({
+      opacity: 0.6
+    });
+  });
+
+  $("#instagram").mouseleave(function(){
+    $("#instagram").animate({
+      opacity: 1
+    });
+  });
+
+  $(".selected").animate({
+    letterSpacing: "+=10px"
+  });
+});
